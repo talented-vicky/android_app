344d49032ffbc18c7d9e7eb75417ffeb527991c1
Victor Otubure <victorotubure7@gmail.com>
2023-02-20 08:40:50 -0800

    added buttons on homepage and image using asset in pubspec for settings and favourite pages

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String btn = 'Click Me!';
+  int ind = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +25,53 @@ class MyApp extends StatelessWidget {
           ),
 
           body: Center(
-              child: ElevatedButton(onPressed: () => {}, child: const Text('Click Me!'))
+            child: ind == 0 ? Container(
+              // width: 400,
+              // width: double.infinity, // takes max width/height space
+              height: double.infinity,
+              color: const Color.fromARGB(255, 77, 235, 247),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [ 
+                  ElevatedButton(
+                    onPressed: () => {
+                      setState(() {
+                        btn = 'Already clicked';
+                      })
+                    }, 
+                    child: Text(btn)
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white
+                    ),
+                    onPressed: () => {
+                      setState(() {
+                        btn = 'Second btn';
+                      })
+                    }, 
+                    child: Text(btn)
+                  )
+                ]
+              ),
+            ) : Image.asset("images/soul-kiss.jpg")
           ),
 
-          bottomNavigationBar: BottomNavigationBar(items: const [
-            BottomNavigationBarItem(label: "Homepage", icon: Icon(Icons.home)),
-            BottomNavigationBarItem(label: 'Favourites', icon: Icon(Icons.favorite)),
-            BottomNavigationBarItem(label: "Settings", icon: Icon(Icons.settings))
-          ]),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(label: "Homepage", icon: Icon(Icons.home)),
+              BottomNavigationBarItem(label: 'Favourites', icon: Icon(Icons.favorite)),
+              BottomNavigationBarItem(label: "Settings", icon: Icon(Icons.settings))
+            ],
+            currentIndex: ind,
+            onTap: (int index) {
+              setState(() {
+                ind = index;
+              });
+            }
+          ),
         ) //the scaffold is the app skeleton (the whitebackground I saw)
         );
   }
